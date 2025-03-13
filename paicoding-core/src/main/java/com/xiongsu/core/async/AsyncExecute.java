b328cbf11808fd0afffaa4595440b1f6ddea6176
suxiao241228 <1229852569@qq.com>
2025-03-13 20:59:00 +0800

    异步执行的切面

---
+++ paicoding-core/src/main/java/com/xiongsu/core/async/AsyncExecute.java
@@ -0,0 +1,38 @@
+package com.xiongsu.core.async;
+
+import java.lang.annotation.*;
+import java.util.concurrent.TimeUnit;
+
+@Target({ElementType.METHOD})
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+public @interface AsyncExecute {
+    /**
+     * 是否开启异步执行
+     *
+     * @return
+     */
+    boolean value() default true;
+
+    /**
+     * 超时时间，默认3s
+     *
+     * @return
+     */
+    int timeOut() default 5;
+
+    /**
+     * 超时时间单位，默认秒，配合上面的 timeOut 使用
+     *
+     * @return
+     */
+    TimeUnit unit() default TimeUnit.SECONDS;
+
+    /**
+     * 当出现超时返回的兜底逻辑,支持SpEL
+     * 如果返回的是空字符串，则表示抛出异常
+     *
+     * @return
+     */
+    String timeOutRsp() default "";
+}
